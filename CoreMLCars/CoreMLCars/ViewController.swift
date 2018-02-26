21dc28da38d2a04fc2dd2e852be7eb054acfd051
Abel Sanchez <Abel.SanchezCustodio@cognizant.com>
2018-02-26 11:26:26 +0100

    Added empty project

---
+++ CoreMLCars/CoreMLCars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMLCars
+//
+//  Created by Sanchez Custodio, Abel (Cognizant) on 26/02/2018.
+//  Copyright © 2018 acsanchezcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
